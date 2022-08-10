edf2cf136aa1c46ef821727364fd176344e2a23f
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-08-10 10:53:19 +0200

    Created ROLE enum and DateConverter

---
+++ app/src/main/java/db/ecommerce/utils/ROLE.java
@@ -0,0 +1,10 @@
+package db.ecommerce.utils;
+
+/**
+ * This enum defines the different roles of a user that can use the application
+ */
+public enum ROLE {
+
+    NESSUNO, CLIENTE, PRODUTTORE, CORRIERE, ADMIN;
+
+}
