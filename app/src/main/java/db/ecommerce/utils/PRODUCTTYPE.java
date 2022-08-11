15d068ac02c0b1572db5bdc1d59eb13d59c23eb2
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-08-11 18:33:30 +0200

    Added method to enums to convert strings into enums

--- app/src/main/java/db/ecommerce/utils/PRODUCTTYPE.java
+++ app/src/main/java/db/ecommerce/utils/PRODUCTTYPE.java
@@ -3,4 +3,12 @@ package db.ecommerce.utils;
 public enum PRODUCTTYPE {
     ALIMENTARE, VESTIARIO;
 
+    public PRODUCTTYPE convert(String s) {
+        if (s.toUpperCase().equals(ALIMENTARE.name())) {
+            return ALIMENTARE;
+        } else {
+            return VESTIARIO;
+        }
+    }
+
 }
--- app/src/main/java/db/ecommerce/utils/TYPEDELIVERY.java
+++ app/src/main/java/db/ecommerce/utils/TYPEDELIVERY.java
@@ -4,4 +4,12 @@ public enum TYPEDELIVERY {
 
     STANDARD, PREMIUM;
 
+    public static TYPEDELIVERY convert(String s) {
+        if (s.toUpperCase().equals(PREMIUM.name())) {
+            return PREMIUM;
+        } else {
+            return STANDARD;
+        }
+    }
+
 }
