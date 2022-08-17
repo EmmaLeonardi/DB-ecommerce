f45a2400df5c13f63056ef5ecc10eccda5bb88a9
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-08-17 18:00:22 +0200

    Added elements to listview and functionality to button Change

--- app/src/main/java/db/ecommerce/controller/ProducerChangesController.java
+++ app/src/main/java/db/ecommerce/controller/ProducerChangesController.java
@@ -1,16 +1,24 @@
 package db.ecommerce.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import db.ecommerce.model.ProducerPK;
+import db.ecommerce.model.ProductPK;
+import db.ecommerce.model.tables.ProductTable;
 import db.ecommerce.utils.ConnectionProvider;
 import db.ecommerce.utils.ConnectionProviderImpl;
 import db.ecommerce.utils.Credentials;
+import db.ecommerce.view.SellingProductMenuImpl;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class ProducerChangesController {
 
@@ -25,13 +33,21 @@ public class ProducerChangesController {
 
     private ProducerPK producer;
 
+    private ProductTable prdTbl;
+
+    private List<ProductPK> showProducts;
+
     public void setProducer(ProducerPK p) {
         this.producer = p;
 
     }
 
     public void change(final Event event) {
-        System.out.println("Change");
+        if (ltvw_products.getSelectionModel().getSelectedIndex() != -1 && !showProducts.isEmpty()) {
+            Stage s = (Stage) btn_change.getScene().getWindow();
+            new SellingProductMenuImpl(s, producer,
+                    showProducts.get(ltvw_products.getSelectionModel().getSelectedIndex()));
+        }
 
     }
 
@@ -46,8 +62,33 @@ public class ProducerChangesController {
     public void initialize() {
         ConnectionProvider c = new ConnectionProviderImpl(Credentials.getUsername(), Credentials.getPassword(),
                 Credentials.getDbname());
-        ltvw_products.setItems(FXCollections.observableList(List.of("a", "b", "c")));
+        this.prdTbl = new ProductTable(c.getMySQLConnection());
+        btn_change.setDisable(true);
+        Platform.runLater(() -> {
+            this.showProducts = prdTbl.allProductsOfProducer(producer);
+            ltvw_products.setItems(FXCollections.observableList(buildProduct(showProducts)));
+            ltvw_products.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
 
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    if (ltvw_products.getSelectionModel().getSelectedIndex() != -1) {
+                        btn_change.setDisable(false);
+                    } else {
+                        btn_change.setDisable(true);
+                    }
+                }
+            });
+        });
+
+    }
+
+    private List<String> buildProduct(List<ProductPK> l) {
+        List<String> list = new ArrayList<>();
+        for (var elem : l) {
+            String s = "" + elem.getCod_prodotto() + " " + elem.getDescrizione() + " " + elem.getMateriale()+" Fabbrica N° "+elem.getCod_fabbrica();
+            list.add(s);
+        }
+        return list;
     }
 
 }
